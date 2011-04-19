16ff3ea7266d58ab46fd8fb15d8e3d5b1770a79f
Carsten Nielsen <carsten@redpill-linpro.com>
2011-04-19 21:12:11 +0200

    Cleaning up a bit.

--- show.sh
+++ show.sh
@@ -1,12 +1,64 @@
 #!/bin/bash
-# git-submodules-presenter script by Carsten Nielsen <carsten@redpill-linpro.com>
 
+## show.sh
+# git-submodules-ascii-demo script by Carsten Nielsen <carsten@redpill-linpro.com>
+
+
+## Settings
+#
 # Set an alternative path for your testsetup here.
 path="/home/carsten/git";
 
 
-## Setup
-if [ "$1" == "setup" ]; then
+## Run
+
+sl=0
+auto=0
+time=0
+ssl=1
+cmd=$1
+message=""
+mes=""
+script=0
+
+##The array of commands
+scriptcmd[0]="cd $path;"
+scriptcmd[15]='ls -a; ls -a public; ls -a local' 
+scriptcmd[16]="cd local; git clone $path/public/project.git; ls -a; ls -a project"
+scriptcmd[18]="cd project; mkdir ext; git submodule add $path/public/extension.git ext/extension"
+scriptcmd[19]='git status; cat .gitmodules';
+scriptcmd[20]='ls -a ext/extension';
+scriptcmd[21]='git add .; git commit -m "Push it!"; git push';
+scriptcmd[23]='git diff HEAD^';
+scriptcmd[25]="cd ..; ls -a; git clone $path/public/project.git others; ls -a others";
+scriptcmd[26]='cd others; ls -a ext/extension'
+scriptcmd[28]='git submodule init; git submodule update'
+scriptcmd[29]="cd ..; rm -Rf others; git clone --recursive $path/public/project.git others; ls others/ext/extension";
+scriptcmd[31]='cd project/ext/extension; echo "A new file in extension." > newfile.txt; ls; git status; git add .; git status; git commit -m "Add a new file."; git log'
+scriptcmd[32]='cd ../..; echo "A new file in project." > newprojectfile.txt; ls; git status; git add . ; git status; git commit -m "Add a new file to project."; git log; git push'
+scriptcmd[33]='cd ../others; git pull; git submodule update'
+scriptcmd[34]='cd ../../public/extension.git; git log'
+
+reader='read -sn1 -p "${mes}(${sl})>" IN';
+
+esc=$(eval 'echo -en "\e"');
+help='echo -e "
+\e[1mh\e[0m or \e[1m?\e[0m - show this help ;-)\n
+\e[1menter\e[0m - start and next\n
+\e[1mf\e[0m - first page is opened\n
+\e[1ms\e[0m - save actual position\n
+\e[1mg\e[0m - go to saved position\n
+\e[1mq\e[0m - quit the presentation\n
+\e[1ma\e[0m - start automatic presentation\n
+\e[1mp\e[0m - pause automatic presentation\n
+\e[1m>\e[0m - open console\n\n
+
+You can use PgUp, PgDn, left, right, up, down and numeric values to jump to slides.\n\n
+
+If you see a "'*'" at the prompt, there are some commands to execute for the actual slide.\n\n
+Press > to open console. If you hit enter, the commands in the brackets will be executed and the output is shown."';
+
+if [ "$1" == "install" ]; then
   if [ ! -d $path ]; then
     echo "Seems the target directory ($path) is not existing!"
     exit;
@@ -46,53 +98,18 @@ elif [ "$1" != "" ]; then
   echo "Welcome! To start just press enter...(or ? for a bit help)";
 else 
   echo "Uuups! You have to define a command with a counting placeholder (?) like:
-  ./show.sh \"cat path/file?.txt\"";
+  > show.sh \"cat path/file?.txt\"
+  or use 
+  > show.sh install
+  to setup the demo environment. You can adjust the installation path in this scriptfile.";
+  echo $help
   exit
 fi
 
-sl=0
-auto=0
-time=0
-ssl=1
-cmd=$1
-message=""
-mes=""
-script=0
-scriptcmd[0]="cd $path;"
-scriptcmd[15]='ls -a; ls -a public; ls -a local' 
-scriptcmd[16]="cd local; git clone $path/public/project.git; ls -a; ls -a project"
-scriptcmd[18]="cd project; mkdir ext; git submodule add $path/public/extension.git ext/extension"
-scriptcmd[19]='git status; cat .gitmodules';
-scriptcmd[20]='ls -a ext/extension';
-scriptcmd[21]='git add .; git commit -m "Push it!"; git push';
-scriptcmd[23]='git diff HEAD^';
-scriptcmd[25]="cd ..; ls -a; git clone $path/public/project.git others; ls -a others";
-scriptcmd[26]='cd others; ls -a ext/extension'
-scriptcmd[28]='git submodule init; git submodule update'
-scriptcmd[29]="cd ..; rm -Rf others; git clone --recursive $path/public/project.git others; ls others/ext/extension";
-scriptcmd[31]='cd project/ext/extension; echo "A new file in extension." > newfile.txt; ls; git status; git add .; git status; git commit -m "Add a new file."; git log'
-scriptcmd[32]='cd ../..; echo "A new file in project." > newprojectfile.txt; ls; git status; git add . ; git status; git commit -m "Add a new file to project."; git log; git push'
-scriptcmd[33]='cd ../others; git pull; git submodule update'
-scriptcmd[34]='cd ../../public/extension.git; git log'
+cd $path;
 
-reader='read -sn1 -p "${mes}(${sl})>" IN';
-
-esc=$(eval 'echo -en "\e"');
-help='echo -e "
-\e[1mh\e[0m or \e[1m?\e[0m - show this help ;-)\n
-\e[1menter\e[0m - start and next\n
-\e[1mf\e[0m - first page is opened\n
-\e[1ms\e[0m - save actual position\n
-\e[1mg\e[0m - go to saved position\n
-\e[1mq\e[0m - quit the presentation\n
-\e[1ma\e[0m - start automatic presentation\n
-\e[1mp\e[0m - pause automatic presentation\n
-\e[1m>\e[0m - open console\n
-
-You can use PgUp, PgDn, left, right, up, down and numeric values to jump to slides.\n"';
-
+##lets go then
 while true; do
-  
   #only if not in script mode...
   if [[ $script == 0 ]]; then
     #input
@@ -131,7 +148,7 @@ while true; do
     esac
   fi
   
-  #only if not in script mode...
+##only if not in script mode...
   if [[ $script == 0 ]]; then
     case "$IN" in
       "a") 
@@ -180,7 +197,7 @@ while true; do
     esac
   fi
   
-  #output
+##output
   if [[ $message != "" ]]; then
     echo $message
     message=""
