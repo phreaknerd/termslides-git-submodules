efff6ce83ce91ca73f9d83d718643577aa0e3cc4
Carsten Nielsen <carsten@redpill-linpro.com>
2011-04-19 14:22:09 +0200

    Initial import to extension.

--- show.sh
+++ show.sh
@@ -1,7 +1,49 @@
 #!/bin/bash
-# Bash presenter script by Carsten Nielsen <carsten@redpill-linpro.com>
+# git-submodules-presenter script by Carsten Nielsen <carsten@redpill-linpro.com>
 
-if [ "$1" != "" ]; then
+# Set the absolute path of your testsetup here.
+# path=/home/carsten/git
+path="~/git"
+
+
+## Setup
+if [ "$1" == "setup" ]; then
+  if [ -d $path ]; then
+    echo "Seems the target directory ($path) is not existing!"
+    exit;
+  elif [ "$(ls $path)" ]; then
+    echo "No setup! Seems the target directory ($path) is not empty!"
+    exit;
+  else
+    cd $path
+    mkdir ./public
+    mkdir ./local
+    cd public
+    git init --bare project.git
+    git init --bare extension.git
+    cd ../local
+    unset GIT_DIR
+    git clone $path/public/project.git
+    echo "The first project file." > project/project.txt
+    cd project
+    git add .
+    git commit -m "Initial import to project."
+    git push origin master
+    unset GIT_DIR
+    cd ..
+    git clone $path/public/extension.git
+    echo "The first extension file." > extension/extension.txt
+    cd extension
+    git add .
+    git commit -m "Initial import to extension."
+    git push origin master
+    cd $path
+    rm -Rf local/*
+    echo
+    echo "OK. Setup done. Now go for the presentation!"
+    exit;
+  fi
+elif [ "$1" != "" ]; then
   echo "Welcome! To start just press enter...(or ? for a bit help)";
 else 
   echo "Uuups! You have to define a command with a counting placeholder (?) like:
@@ -17,7 +59,25 @@ cmd=$1
 message=""
 mes=""
 script=0
+scriptcmd[0]="cd $path;"
+scriptcmd[15]='ls -a; ls -a public; ls -a local' 
+scriptcmd[16]="cd local; git clone $path/public/project.git; ls -a; ls -a project"
+scriptcmd[18]="cd project; mkdir ext; git submodule add $path/public/extension.git ext/extension"
+scriptcmd[19]='git status; cat .gitmodules';
+scriptcmd[20]='ls -a ext/extension';
+scriptcmd[21]='git add .; git commit -m "Push it!"; git push';
+scriptcmd[23]='git diff HEAD^';
+scriptcmd[25]="cd ..; ls -a; git clone $path/public/project.git others; ls -a others";
+scriptcmd[26]='cd others; ls -a ext/extension'
+scriptcmd[28]='git submodule init; git submodule update'
+scriptcmd[29]="cd ..; rm -Rf others; git clone --recursive $path/public/project.git others; ls others/ext/extension";
+scriptcmd[31]='cd project/ext/extension; echo "A new file in extension." > newfile.txt; ls; git status; git add .; git status; git commit -m "Add a new file."; git log'
+scriptcmd[32]='cd ../..; echo "A new file in project." > newprojectfile.txt; ls; git status; git add . ; git status; git commit -m "Add a new file to project."; git log; git push'
+scriptcmd[33]='cd ../others; git pull; git submodule update'
+scriptcmd[34]='cd ../../public/extension.git; git log'
+
 reader='read -sn1 -p "${mes}(${sl})>" IN';
+
 esc=$(eval 'echo -en "\e"');
 help='echo -e "
 \e[1mh\e[0m or \e[1m?\e[0m - show this help ;-)\n
@@ -28,6 +88,7 @@ help='echo -e "
 \e[1mq\e[0m - quit the presentation\n
 \e[1ma\e[0m - start automatic presentation\n
 \e[1mp\e[0m - pause automatic presentation\n
+\e[1m>\e[0m - open console\n
 
 You can use PgUp, PgDn, left, right, up, down and numeric values to jump to slides.\n"';
 
@@ -36,6 +97,9 @@ while true; do
   #only if not in script mode...
   if [[ $script == 0 ]]; then
     #input
+    if [[ -n "${scriptcmd[$sl]}" ]]; then 
+      mes=$mes" [*]" 
+    fi
     eval $reader
     mes=""
     case "$IN" in
@@ -51,6 +115,7 @@ while true; do
           esac
         done;;
       ">")
+        clear
         script=1
         ssl=$sl;;
       [0-9]*)
@@ -65,7 +130,6 @@ while true; do
           esac
         done;;
     esac
-    echo $IN
   fi
   
   #only if not in script mode...
@@ -122,7 +186,29 @@ while true; do
     echo $message
     message=""
   elif [[ $script == 1 ]]; then
-    read -p "me@local$>" IN
+    echo -n "[1m"
+    if [ -n "${scriptcmd[$ssl]}" ]; then
+      read -p "me@local:$PWD $ [0m(${scriptcmd[$ssl]}) [1m>" IN
+    else
+      read -p "me@local:$PWD $> " IN
+    fi
+    if [ -z "$IN" ]; then
+      OIFS=$IFS
+      IFS=';'
+      arr="${scriptcmd[$ssl]}"
+      for x in $arr
+      do
+        echo
+        echo -n "[1m"
+        echo "me@local:$PWD $> $x"
+        echo -n "[0m"
+        eval "${x}"
+      done
+      IFS=$OIFS
+      scriptcmd[$ssl]=''
+      IN=""
+    fi
+    echo -n "[0m"
     case $IN in
       q)
         script=0
